d0232f2598117e1424502321e1421aad407b5f2a
khyatijain15 <khyati31574@gmail.com>
2025-06-10 00:12:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Skills from './components/skills';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 import UIUX from './components/UIUX';
 import Certificates from './components/Certificates';
